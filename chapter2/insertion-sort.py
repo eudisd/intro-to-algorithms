a2168a7fe2b794590450d30131580205889ea5a2
Eudis Duran <eudisd@gmail.com>
2013-08-29 08:39:11 -0400

    Working on chapter 2's insertion sort

---
+++ chapter2/insertion-sort.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+import random
+
+
+def insertion_sort(A):
+    return
+
+def main():
+    length = 10
+    l = [random.randint(0, length) for i in range(length)]
+    print "Before"
+    print l
+    print "After"
+    print insertion_sort(l)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
