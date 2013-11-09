2e6d459403704e38ffc6fd0ed8cf1841e7bf994e
Eudis Duran <eudisd@gmail.com>
2013-11-09 00:41:53 -0500

    basic insertion sort algo

--- chapter2/insertion-sort.py
+++ chapter2/insertion-sort.py
@@ -1,18 +1,26 @@
-#! /usr/bin/env python
-
-import random
-
+# /usr/bin/env python
 
 def insertion_sort(A):
-    return
+    for j in range(1, len(A)):
+        key = A[j]
+
+        i = j - 1
+
+        while i > -1 and A[i] > key:
+            A[i + 1] = A[i]
+            i -= 1
+        A[i + 1] = key
+
+    return A
 
 def main():
-    length = 10
-    l = [random.randint(0, length) for i in range(length)]
-    print "Before"
+    l = [5, 2, 4, 6, 1, 3]
+
     print l
-    print "After"
-    print insertion_sort(l)
+
+    l = insertion_sort(l)
+
+    print l
 
     return
 
