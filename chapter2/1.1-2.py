45f6cb4457316009e0a9315e20c24354b26d812c
Eudis Duran <eudisd@gmail.com>
2013-11-09 00:47:07 -0500

    Exercise 1.1-2

---
+++ chapter2/1.1-2.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+
+def dec_insertion_sort(A):
+    for j in range(1, len(A)):
+        key = A[j]
+        i = j - 1
+        while i > -1 and A[i] < key:
+            A[i + 1] = A[i]
+            i -= 1
+        A[i + 1] = key
+    return A
+
+def main():
+    l = [5, 2, 0, 3, 8, 7]
+
+    print l
+
+    l = dec_insertion_sort(l)
+
+    print l
+
+
+if __name__ == "__main__":
+    main()
