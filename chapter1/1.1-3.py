2abdffe27ebdc0d1689900b4850d4eb529767e96
Eudis Duran <eudisd@gmail.com>
2013-11-10 12:45:00 -0500

    problem desc

--- chapter1/1.1-3.py
+++ chapter1/1.1-3.py
@@ -1,5 +1,12 @@
 #! /usr/bin/env python
 
+"""
+    Write code for linear search, which scans through the sequence
+    looking for v.
+    Input: A sequence of n numbers A = <a1, a2,...,an> and a value v.
+    Output: An index i such that v = A[i] or the special value NIL if v
+    does not appear in A.
+"""
 
 def linear_search(A, v):
     for i in range (len(A)):
