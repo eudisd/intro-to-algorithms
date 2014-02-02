37a1625b364396deccb3cc60888316815b216058
Eudis Duran <eudisd@gmail.com>
2014-02-02 10:59:16 -0500

    problem stub

---
+++ chapter1/1.1-4.py
@@ -0,0 +1,12 @@
+""" Consider the problem of adding two n-bit binary integers
+    stored in two n-element arrays A and B.  The sum of the 
+    two integers should be stored in binary form in an 
+    (n + 1)-element array C.  Write code for adding the two
+    integers
+"""
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
